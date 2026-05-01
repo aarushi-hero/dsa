1db83fe9927e438333f7634c11f4f8c264618eac
aarushi-hero <158171241+aarushi-hero@users.noreply.github.com>
2026-05-01 10:26:17 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -34,3 +34,40 @@ class Solution:
                     right -= 1
 
         return res
+
+
+
+
+
+
+
+
+
+#
+class Solution:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+
+        nums.sort()
+        closest_sum = nums[0] + nums[1] + nums[2]   # initialize properly
+
+        for i in range(len(nums) - 2):
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                # update closest sum
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum = curr_sum
+
+                if curr_sum == target:
+                    return curr_sum   # best possible answer
+
+                elif curr_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return closest_sum
